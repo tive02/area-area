ddff21aab8a8abf2a4b02a0e75d733e7528d1345
Jorge Moreno <jsmorenop@unal.edu.co>
2021-04-17 19:10:06 -0500

    Correccion Total del Header

---
+++ .history/components/molecules/MenuPublic_20210417190453.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Link from "next/link";
+
+
+export const MenuPublic = () => {
+  return (
+    <nav>
+      <ul className="invisible md:visible md:flex md:flex-row md:justify-around">
+        <li>
+          <Link className="className="px-4 py-2 mt-2 text-lg font-semibold text-black-400 
+          md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-green-400 focus:bg-green-400 
+          focus:outline-none focus:shadow-outline" href="/">
+            Inicio
+          </Link>
+        </li>
+        <li>
+          <Link className="" href="/tags/Tags">
+            Temas
+          </Link>
+        </li>
+        <li>
+          <Link href="/about/About">Nuestro Equipo</Link>
+        </li>
+        <li>
+          <Link
+            className="px-4 py-2 mt-2 text-lg font-semibold text-black-400 
+          md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-green-400 focus:bg-green-400 
+          focus:outline-none focus:shadow-outline"
+            href="/contribution/Contribution">
+            ¿Como contribuir?
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default MenuPublic;
--- .history/components/molecules/MenuPublic_20210417190640.jsx
+++ .history/components/molecules/MenuPublic_20210417190640.jsx
@@ -6,13 +6,13 @@ export const MenuPublic = () => {
     <nav>
       <ul className="invisible md:visible md:flex md:flex-row md:justify-around">
         <li
-          className="px-4 py-2 mt-2 text-sm font-semibold text-black-400 
+          className="px-4 py-2 mt-2 text-lg font-semibold text-black-400 
           md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-green-400 focus:bg-green-400 
           focus:outline-none focus:shadow-outline">
           <Link href="/">Inicio</Link>
         </li>
         <li
-          className="px-4 py-2 mt-2 text-sm font-semibold text-black-400 
+          className="px-4 py-2 mt-2 text-lg font-semibold text-black-400 
           md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-green-400 focus:bg-green-400 
           focus:outline-none focus:shadow-outline">
           <Link className="" href="/tags/Tags">
@@ -20,7 +20,7 @@ export const MenuPublic = () => {
           </Link>
         </li>
         <li
-          className="px-4 py-2 mt-2 text-sm font-semibold text-black-400 
+          className="px-4 py-2 mt-2 text-lg font-semibold text-black-400 
           md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-green-400 focus:bg-green-400 
           focus:outline-none focus:shadow-outline">
           <Link href="/about/About">Nuestro Equipo</Link>
