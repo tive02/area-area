7da7077940ce9443e6b2703dfe3ca445f67d7b2f
Jorge Moreno <jsmorenop@unal.edu.co>
2021-04-27 20:00:55 -0500

    Archivos de Configuracion de firebase

---
+++ .history/firebase/config_20210427195841.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA47WLglrHOyJzI7rUsrwXIx5vjWZG-hos",
+  authDomain: "de-area-a-area.firebaseapp.com",
+  databaseURL: "https://de-area-a-area.firebaseio.com",
+  projectId: "de-area-a-area",
+  storageBucket: "de-area-a-area.appspot.com",
+  messagingSenderId: "468073735047",
+  appId: "1:468073735047:web:7b1b77024700e6ee08e922",
+  measurementId: "G-M4BX9HNEVC",
+};
+
+export default firebaseConfig;
