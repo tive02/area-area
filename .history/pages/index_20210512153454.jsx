ced8e4fa33d3016624d06edef04b8d9b04cf6aec
Jorge Moreno <jsmorenop@unal.edu.co>
2021-05-12 15:40:37 -0500

    Modificacion del index colo el css y el contenido

--- .history/pages/index_20210512153454.jsx
+++ .history/pages/index_20210512153454.jsx
@@ -26,27 +26,28 @@ export default function Home() {
               <img
                 src="https://images.unsplash.com/photo-1427751840561-9852520f8ce8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60"
                 class="rounded-md object-cover w-full h-64"
-              />
-              <span class="text-green-700 text-sm hidden md:block mt-4">
-                Technology
-              </span>
-              <h1 class="text-gray-800 text-4xl font-bold mt-2 mb-2 leading-tight">
-                Ignorant branched humanity led now marianne too.
-              </h1>
-              <p class="text-gray-600 mb-4">
-                Necessary ye contented newspaper zealously breakfast he
-                prevailed. Melancholy middletons yet understood decisively boy
-                law she. Answer him easily are its barton little. Oh no though
-                mother be things simple itself. Oh be me, sure wise sons, no.
-                Piqued ye of am spirit regret. Stimulated discretion impossible
-                admiration in particular conviction up.
-              </p>
-              <a
-                href="./blog.html"
-                class="inline-block px-6 py-3 mt-2 rounded-md bg-green-700 text-gray-100"
               >
-                Read more
-              </a>
+                <span class="text-green-700 text-sm hidden md:block mt-4">
+                  Technology
+                </span>
+                <h1 class="text-gray-800 text-4xl font-bold mt-2 mb-2 leading-tight">
+                  Ignorant branched humanity led now marianne too.
+                </h1>
+                <p class="text-gray-600 mb-4">
+                  Necessary ye contented newspaper zealously breakfast he
+                  prevailed. Melancholy middletons yet understood decisively boy
+                  law she. Answer him easily are its barton little. Oh no though
+                  mother be things simple itself. Oh be me, sure wise sons, no.
+                  Piqued ye of am spirit regret. Stimulated discretion
+                  impossible admiration in particular conviction up.
+                </p>
+                <a
+                  href="./blog.html"
+                  class="inline-block px-6 py-3 mt-2 rounded-md bg-green-700 text-gray-100"
+                >
+                  Read more
+                </a>
+              </img>
             </div>
 
             {/* <!-- sub-main posts --> */}
