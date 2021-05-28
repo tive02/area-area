53079b2aff9f5e76bbd9130c203c31f1dc50f1f6
Jorge Moreno <jsmorenop@unal.edu.co>
2021-05-28 18:54:16 -0500

    prueba menu mobile, para el modal se renseriza bien

--- .history/components/molecules/ModalLogin_20210528185123.jsx
+++ .history/components/molecules/ModalLogin_20210528185123.jsx
@@ -15,10 +15,7 @@ export default function ModalLogin() {
 
       {showModal ? (
         <>
-          <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed 
-          lg:inset-0 inset-y-0 left-0 z-50 outline-none focus:outline-none"
-          >
+          <div className="lg:justify-center lg:items-center lg:flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-screen lg:w-auto my-6 mx-auto max-w-sm">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
